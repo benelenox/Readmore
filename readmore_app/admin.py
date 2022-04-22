a359d645b5ac9fa2bb0354f8975a476d4a3b6d2d
Ben Lenox <lenox.ben@gmail.com>
2022-04-22 19:10:37 -0400

    Registered all models with django admin

--- readmore_app/admin.py
+++ readmore_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(UserExt)
+admin.site.register(Notification)
+admin.site.register(Club)
+admin.site.register(ClubBook)
+admin.site.register(ReadingLogBook)
+admin.site.register(Chat)
+admin.site.register(ClubChat)
+admin.site.register(PM)
+admin.site.register(Post)
+admin.site.register(ClubPost)
+admin.site.register(ProfilePost)
+admin.site.register(ReviewPost)
+admin.site.register(BookForumPost)
+admin.site.register(Meeting)
